0bc358146c5e4219738a807fc2d18a5f73c56311
Biasiolo <biasiolorafael@gmail.com>
2025-02-12 01:38:13 -0300

    initial

--- src/components/ContactList.styled.ts
+++ src/components/ContactList.styled.ts
@@ -86,8 +86,8 @@ export const ContactInfo = styled.span`
   strong {
 font-size: ${({ theme }) => theme.fontSizes.lg};
 font-weight: bold; /* Opcional, se quiser reforçar o destaque */
-color: #f3f2f7;
-text-shadow: 0 2px 4px #6a696d;
+color: #d3d3d3;
+text-shadow: 0 2px 6px #6a696d;
 }
 `;
 
